7d4032729c37c19b2d7ca9fa96b4231728a4cfdf
sumanthegdegithub <reachsumanthegde@gmail.com>
2024-07-06 10:20:12 +0530

    changed python version

--- api/app.py
+++ api/app.py
@@ -40,7 +40,7 @@ api_router = APIRouter()
 
 
 subprocess.call(["ollama", "serve", "&&", "sleep", "10", "&&", "ollama", "pull", "phi3"])
-print(subprocess.check_output(['ollama', 'list']).splitlines())
+print(subprocess.check_output(['ollama', 'list'], text=True))
 
 
 
