38af42c21862f06592ff7e59ceba60dde9903b11
sumanthegdegithub <reachsumanthegde@gmail.com>
2024-07-06 11:09:25 +0530

    changed python version

--- api/app.py
+++ api/app.py
@@ -39,7 +39,7 @@ app = FastAPI()
 api_router = APIRouter()
 
 
-print(subprocess.call(["ollama", "serve", "&&", "sleep", "10", "&&", "ollama", "pull", "phi3", "&&", "ollama", "list"]))
+print(subprocess.call(["ollama", "serve"]))
 
 
 backend_ip = os.getenv("backend_ip")
