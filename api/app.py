5f618cb8e1a232e064e6b0479ac8462fd7a57384
sumanthegdegithub <reachsumanthegde@gmail.com>
2024-07-12 21:29:09 +0530

    added cuda visible devices

--- api/app.py
+++ api/app.py
@@ -33,6 +33,8 @@ import os
 from db import get_connection
 from dotenv import load_dotenv 
 import subprocess
+from search_model import nnlm, knn, preprocessing, get_orders_from_nnlm
+
 load_dotenv()
 
 app = FastAPI()
@@ -41,6 +43,7 @@ api_router = APIRouter()
 def ollama():
     os.environ['OLLAMA_HOST'] = 'http://0.0.0.0:11434/'
     os.environ['OLLAMA_ORIGINS'] = '*'
+    os.environ['CUDA_VISIBLE_DEVICES'] = 0
     subprocess.Popen(["ollama", "serve"])
 
 ollama()
@@ -98,16 +101,18 @@ async def run_llm(USER_ID, text, user_selected_product = '', user_confirmation =
     if text == '':
         return {'user_selected_product': '', 'orders' : None, 'message' : 'Sorry I could not understand :( Could you please rephrase it?', 'resp_type' : None, 'function_to_call' : None }
     
-    user_chats, order_id = get_user_chats(USER_ID, prompt_template)
+    
+    if user_selected_product == '':
+        order_ids = get_orders(text)
+        if len(order_ids) == 0:
+            order_ids = get_orders_from_nnlm(text, USER_ID, nnlm, knn)
+        if len(order_ids) == 0:
+            user_chats, order_id = get_user_chats(USER_ID, prompt_template)
+        
     confirmation = user_confirmation
     
-    
-    if (user_selected_product == '') & (order_id != ''):
-        user_selected_product = order_id
      
     if user_selected_product == '':
-        
-        order_ids = get_orders(text)
         
         if len(order_ids) > 0:
             if len(order_ids) > 1:
